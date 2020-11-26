cefe550003fe53d3c0fafddd8ac1e6a400baf8c6
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-11-27 00:25:58 +0200

    Add basic DB operations

--- app/events_service/database.py
+++ app/events_service/database.py
@@ -25,3 +25,42 @@ class MongoDBConnection(metaclass=Singleton):
 
     def get_collection(self, collection_name: str):
         return self.database.get_collection(collection_name)
+
+
+class EventsCollectionOperations:
+    def __init__(self):
+        self.collection = MongoDBConnection().get_collection('events')
+
+    async def get_event(self, event_id: int) -> dict:
+        event = await self.collection.find_one({"id": event_id})
+        if event:
+            return event
+
+        return {}
+
+    async def add_event(self, event_data: dict) -> bool:
+        await self.collection.insert_one(event_data)
+        return True
+
+    async def update_event(self, event_data: dict) -> bool:
+        event_id = event_data.get('id', '')
+        if not event_id:
+            return False
+
+        event = await self.collection.find_one({"id": event_id})
+        if event:
+            new_event = await self.collection.update_one(
+                {"id": event_id}, {'$set': event_data})
+            if new_event:
+                return True
+            return False
+
+        return False
+
+    async def delete_event(self, event_id: int) -> bool:
+        event = await self.collection.find_one({"id": event_id})
+        if event:
+            await self.collection.delete_one({"id": event_id})
+            return True
+
+        return False
